afc22ccdbd6f1d40daedeca3cf1b17c9c2efccd6
mlhilhan <muh.mlhilhan@gmail.com>
2025-04-07 18:37:16 +0300

    fx added other languages

--- src/config/appLanguages.js
+++ src/config/appLanguages.js
@@ -1,10 +1,18 @@
 const appLanguages = [
-  { value: "tr", label: "Türkçe" },
-  { value: "en", label: "English" },
-  { value: "ar", label: "عربية" },
-  { value: "es", label: "Español" },
-  { value: "ru", label: "русский язык" },
-  { value: "zh", label: "中文" },
+  { value: "en", label: "English" }, // İngilizce
+  { value: "zh", label: "中文" }, // Çince
+  { value: "hi", label: "हिन्दी" }, // Hintçe
+  { value: "es", label: "Español" }, // İspanyolca
+  { value: "ar", label: "عربية" }, // Arapça
+  { value: "fr", label: "Français" }, // Fransızca
+  { value: "pt", label: "Português" }, // Portekizce
+  { value: "ru", label: "русский язык" }, // Rusça
+  { value: "id", label: "Bahasa Indonesia" }, // Endonezce
+  { value: "ja", label: "日本語" }, // Japonca
+  { value: "de", label: "Deutsch" }, // Almanca
+  { value: "ko", label: "한국어" }, // Korece
+  { value: "it", label: "Italiano" }, // İtalyanca
+  { value: "tr", label: "Türkçe" }, // Türkçe
 ];
 
 export default appLanguages;
